af276567705642f5d765b36112bed719d4a2359c
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-07-12 18:33:28 -0300

    Removendo a URL do escopo global e colocando diretamente no fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,9 @@
-const URL_CATEGORIES = 'https://api.mercadolibre.com/sites/MLB/categories';
-
 export async function getCategories() {
-  return fetch(URL_CATEGORIES)
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((data) => data.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const URL_CATEGORY_AND_QUERY = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}}`;
-  return fetch(URL_CATEGORY_AND_QUERY)
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}}`)
     .then((data) => data.json());
 }

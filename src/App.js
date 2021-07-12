20a3f184f3a7be044768ef1d17a3debd8f59de7a
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-12 17:31:44 -0300

    terminando ProductList e criando SearchBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import ProductList from './components/ProductList';
 import './App.css';
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-       
+        <ProductList />
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      empty: true,
+    }
+  }
   render() {
+    const { empty } = this.state;
     return (
       <div>
-
+        { empty ? <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p> : 'Lista futura' }
       </div>
     );
   }

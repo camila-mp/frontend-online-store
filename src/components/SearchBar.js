f6933f4dd54eeb8b36588d5a7ce25c27e41b5568
Roberval Filho <rslfilho@gmail.com>
2021-07-12 20:19:09 -0300

    Implementando State e HandleChange em SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      search: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { search } = this.state;
+
     return (
-      <input type="text" placeholder="tem nada aqui ainda" />
+      <form>
+        <input
+          name="search"
+          value={ search }
+          data-testid="query-input"
+          type="text"
+          placeholder="tem nada aqui ainda"
+          onChange={ this.handleChange }
+        />
+        <button data-testid="query-button" type="submit">Procurar</button>
+      </form>
     );
   }
 }

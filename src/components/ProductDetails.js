4b903100daa427858b59f6666921c4495ebebb74
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-13 17:22:27 -0300

    adicinando componente Product Details

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default ProductDetails;

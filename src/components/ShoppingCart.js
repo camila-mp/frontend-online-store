e7b8449e3dbdd1108fe159d68ce2a946406bb2f2
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-13 10:52:14 -0300

    Criando estado e condição para ShoppingCart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      empyt: true,
+    };
+  }
+
   render() {
+    const { empyt } = this.state;
+    if (empyt) {
+      return (
+        <div>
+          <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+        shoppingcart
       </div>
     );
   }

ae4af2a1c33bac0e1d98403dd468c7d974e01bc8
Roberval Filho <rslfilho@gmail.com>
2021-07-13 19:13:10 -0300

    Adicionando links dos colegas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ class Footer extends Component {
       <div className="footer">
         <p>
           <span>Desenvolvido por </span>
-          Camila,
-          Felipe,
-          Gabriel,
+          <a href="https://www.linkedin.com/in/camila-malvessi-6b3367110/" target="_blank" rel="noopener noreferrer"> Camila, </a>
+          <a href="https://www.linkedin.com/in/felipelouzeiro/" target="_blank" rel="noopener noreferrer"> Felipe, </a>
+          <a href="https://linkedin.com/in/gabrielmedeiro5" target="_blank" rel="noopener noreferrer"> Gabriel, </a>
           Gabriela,
-          Guilherme e
+          <a href="https://www.linkedin.com/in/guilherme-hermenegildo-junior/" target="_blank" rel="noopener noreferrer"> Guilherme e </a>
           <a href="https://www.rslfilho.com.br/" target="_blank" rel="noopener noreferrer"> Roberval</a>
           <span> usando React.js - Bloco 14 - </span>
           <a href="http://www.betrybe.com.br/" target="_blank" rel="noopener noreferrer">Trybe</a>

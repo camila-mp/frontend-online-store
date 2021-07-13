2a2d376d4e70e0b4286399c74c03abf4465de723
Roberval Filho <rslfilho@gmail.com>
2021-07-13 09:41:30 -0300

    Estilizando Até o requisito 02

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './SearchBar.css';
 
 class SearchBar extends React.Component {
   render() {
     return (
-      <input type="text" placeholder="tem nada aqui ainda" />
+      <header>
+        <img src="https://logospng.org/download/mercado-livre/logo-mercado-livre-256.png" alt="Logo Mercado Livre" />
+        <input type="text" placeholder="tem nada aqui ainda" />
+      </header>
     );
   }
 }

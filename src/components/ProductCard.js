072459dd40bdcd3b94b986190f2ed01b4992553f
Roberval Filho <rslfilho@gmail.com>
2021-07-12 22:35:00 -0300

    Voltando o link de imagem para mais qualidade

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 class ProductCard extends React.Component {
   render() {
     const { product } = this.props;
-    const { title, thumbnail, price } = product;
+    const { title, thumbnail_id: id, price } = product;
 
     return (
       <div data-testid="product">
         <p>{ title }</p>
-        <img src={ thumbnail } alt="Imagem do Produto" />
+        <img src={ `https://http2.mlstatic.com/D_NQ_NP_${id}-W.webp` } alt="Imagem do Produto" />
         <p>{ price }</p>
       </div>
     );
@@ -19,7 +19,7 @@ class ProductCard extends React.Component {
 ProductCard.propTypes = {
   product: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    thumbnail: PropTypes.string.isRequired,
+    thumbnail_id: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
   }).isRequired,
 };

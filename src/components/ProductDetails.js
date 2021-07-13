32d9598c8c9edc3fa8012e31673a5209531274b4
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-13 17:46:24 -0300

    adicionando os detalhes e imagem  no ProductDetails

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,10 +3,18 @@ import React from 'react';
 class ProductDetails extends React.Component {
   render() {
     const { product } = this.props;
-    const { title, price } = product;
+    const { title, price, thumbnail_id: id } = product;
     return (
       <div>
         <h3>{ `${title} - R$${price}` } </h3>
+        <div>
+          <div>
+            <img src={ `https://http2.mlstatic.com/D_NQ_NP_${id}-W.webp` } alt="Imagem do Produto" />
+          </div>
+          <div>
+            infos
+          </div>
+        </div>
       </div>
     );
   }

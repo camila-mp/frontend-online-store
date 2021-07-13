9195bb85b19119502b05eeb347e0097c5d27f2ca
Roberval Filho <rslfilho@gmail.com>
2021-07-13 17:35:37 -0300

    Estilizando Butão do carrinho

--- src/components/BtnCart.js
+++ src/components/BtnCart.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './BtnCart.css';
 
 class BtnCart extends Component {
   render() {
     return (
-      <Link to="/ShoppingCart">
+      <Link to="/ShoppingCart" className="shopping-cart-button">
         <i className="bi bi-cart2" data-testid="shopping-cart-button" />
       </Link>
     );

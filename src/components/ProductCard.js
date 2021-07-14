861e3711ff6bff17ce3ba28d5df29de5786f6b7d
camila-mp <ca.malvp@gmail.com>
2021-07-13 21:23:10 -0300

    adiciona botao (adiciona ao carrinho), cria estado e cria handleClick() dentro de ProductCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      selectedProduct: {},
+    };
+  }
+
+  handleClick({ target: { name }) {
+    const { getState } = this.props;
+
+    getState(name, )
+  }
+
   render() {
     const { product } = this.props;
     const { title, thumbnail_id: id, price } = product;
@@ -11,6 +28,16 @@ class ProductCard extends React.Component {
         <p>{ title }</p>
         <img src={ `https://http2.mlstatic.com/D_NQ_NP_${id}-W.webp` } alt="Imagem do Produto" />
         <p>{ price }</p>
+        <Link to="/ShoppingCart">
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            data-testid="product-add-to-cart "
+            name="selectedProduct"
+          >
+            Adicionar ao carrinho
+          </button>
+        </Link>
       </div>
     );
   }

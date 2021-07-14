28a46cc0532d941777503bd67baf3c22d1d42b0f
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-07-14 17:48:03 -0300

    Adicionando link voltar no componente Details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductDetails extends React.Component {
   constructor(props) {
@@ -58,6 +59,7 @@ class ProductDetails extends React.Component {
             >
               Adicione ao carrinho
             </button>
+            <Link to="/search">Voltar</Link>
           </div>
         </div>
       </div>

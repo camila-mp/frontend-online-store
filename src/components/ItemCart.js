1583e94fe19ed1d5838a708dd7c09c40c8593c8c
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-07-15 18:37:45 -0300

    resolvendo conflitos

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -1,73 +1,73 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class ShoppingCart extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       quantity: 1,
-//     };
-//     this.decreaseQuant = this.decreaseQuant.bind(this);
-//     this.increaseQuant = this.increaseQuant.bind(this);
-//   }
+class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1,
+    };
+    this.decreaseQuant = this.decreaseQuant.bind(this);
+    this.increaseQuant = this.increaseQuant.bind(this);
+  }
 
-//   decreaseQuant() {
-//     this.setState(() => ({
-//       quantity: quantity - 1,
-//     }));
-//   }
+  decreaseQuant() {
+    this.setState(() => ({
+      quantity: quantity - 1,
+    }));
+  }
 
-//   increaseQuant() {
-//     this.setState(() => ({
-//       quantity: quantity + 1,
-//     }));
-//   }
+  increaseQuant() {
+    this.setState(() => ({
+      quantity: quantity + 1,
+    }));
+  }
 
-//   render() {
-//     const { quantity } = this.state;
-//     const { item } = this.props;
-//     const { title, price, thumbnail_id: id } = item;
-//     return (
-//       <div data-testid="product">
-//         <h4 data-testid="shopping-cart-product-name">{ title }</h4>
-//         <p>{ price }</p>
-//         <img src={ thumbnail_id: id } alt={ title } />
-//         <button
-//           data-testid="product-decrease-quantity"
-//           type="button"
-//           onClick={ this.decreaseQuant }
-//         >
-//           -
-//         </button>
-//         <p data-testid="shopping-cart-product-quantity">
-//           { quantity }
-//         </p>
-//         <button
-//           data-testid="product-increase-quantity"
-//           type="button"
-//           onClick={ this.increaseQuant }
-//         >
-//           +
-//         </button>
-//         <button type="button"
-//         >
-//           X
-//         </button>
-//         <button type="button"
-//         >
-//           Finalizar Compra
-//         </button>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    const { quantity } = this.state;
+    const { item } = this.props;
+    const { title, price, thumbnail_id: id } = item;
+    return (
+      <div data-testid="product">
+        <h4 data-testid="shopping-cart-product-name">{ title }</h4>
+        <p>{ price }</p>
+        <img src={ thumbnail_id: id } alt={ title } />
+        <button
+          data-testid="product-decrease-quantity"
+          type="button"
+          onClick={ this.decreaseQuant }
+        >
+          -
+        </button>
+        <p data-testid="shopping-cart-product-quantity">
+          { quantity }
+        </p>
+        <button
+          data-testid="product-increase-quantity"
+          type="button"
+          onClick={ this.increaseQuant }
+        >
+          +
+        </button>
+        <button type="button"
+        >
+          X
+        </button>
+        <button type="button"
+        >
+          Finalizar Compra
+        </button>
+      </div>
+    );
+  }
+}
 
-// ItemCart.propTypes = {
-//   item: PropTypes.shape({
-//     title: PropTypes.string.isRequired,
-//     price: PropTypes.number.isRequired,
-//     thumbnail_id: PropTypes.string.isRequired,
-//   }),
-// }.isRequired;
+ItemCart.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    thumbnail_id: PropTypes.string.isRequired,
+  }),
+}.isRequired;
 
-// export default ItemCart;
+export default ItemCart;

a217a59cc6baf27332f0004bf47327008235536c
camila-mp <ca.malvp@gmail.com>
2021-10-03 21:14:42 -0300

    Updates

---
+++ precache-manifest.b8a8634ce2fb27bbb000b6953ddcd906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2a6f364972b5926371aaff3a5bf1d6",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "fce3363d34700c6b9e19",
+    "url": "/frontend-online-store/static/css/main.448ae2a1.chunk.css"
+  },
+  {
+    "revision": "15fff0f437d93939bb94",
+    "url": "/frontend-online-store/static/js/2.965e01c3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.965e01c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce3363d34700c6b9e19",
+    "url": "/frontend-online-store/static/js/main.61ce4faa.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  }
+]);
